7763e237ff7d1f4dfc14e2a931c5b40da878bbb1
Pavlova-Polina <pavlova.polina@chnu.edu.ua>
2024-05-17 08:09:25 +0300

    task 2

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -12,28 +12,6 @@ using namespace std;
 int main()
 {
     SetConsoleOutputCP(1251);
-    cout << "OOP. Template for laboratory work #2.\n";
-
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = cin.get();
-        
-        cin.get();
-
-        switch (ch) {
-        case '1': task1();   break;
-        case '2': task1();   break;
-        case '3': task1();   break;
-        case '4': task1();   break;
-		case '5':  Examples(); break;
-		case '6': return 0;
-	    }
-        cout << " Press any key and enter\n";
-        ch = cin.get();
-    } while (ch != '6');
-
     return 0;
 }
 void task1() {
